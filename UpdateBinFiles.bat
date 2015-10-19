2ab60be40bfcf6b1f498d1f8bfe9851ab8f34a25
WSDOTbricer <WSDOTbricer>
2015-10-19 20:31:06 +0000

    *** empty log message ***

--- UpdateBinFiles.bat
+++ UpdateBinFiles.bat
@@ -4,5 +4,5 @@ SET BINTARGET=bin
 SET REGFREECOM=\ARP\BridgeLink\RegFreeCOM
 
 
-copy /Y %REGFREECOM%\Win32\Release\OpenBridgeML.dll	%BINTARGET%\Win32\
-copy /Y %REGFREECOM%\x64\Release\OpenBridgeML.dll	%BINTARGET%\x64\
+xcopy /Y /d %REGFREECOM%\Win32\Release\OpenBridgeML.dll	%BINTARGET%\Win32\
+xcopy /Y /d %REGFREECOM%\x64\Release\OpenBridgeML.dll	%BINTARGET%\x64\
