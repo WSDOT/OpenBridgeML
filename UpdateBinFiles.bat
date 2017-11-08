2e289cf16da5bf25feb96a96cfa1852a97ff4f75
WSDOTBriceR <WSDOTBriceR>
2017-11-08 19:25:45 +0000

    *** empty log message ***

--- UpdateBinFiles.bat
+++ UpdateBinFiles.bat
@@ -4,5 +4,4 @@ SET BINTARGET=bin
 SET REGFREECOM=\ARP\BridgeLink\RegFreeCOM
 
 
-xcopy /Y /d %REGFREECOM%\Win32\Release\OpenBridgeML.dll	%BINTARGET%\Win32\
 xcopy /Y /d %REGFREECOM%\x64\Release\OpenBridgeML.dll	%BINTARGET%\x64\
